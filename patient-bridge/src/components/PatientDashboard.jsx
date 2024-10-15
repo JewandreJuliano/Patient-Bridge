055c9d40b980bea5fd45c957bf01643481abefdf
Jewandre Witbooi <s223172057@mandela.ac.za>
2024-10-15 15:34:55 +0200

    Minor Changes

--- patient-bridge/src/components/PatientDashboard.jsx
+++ patient-bridge/src/components/PatientDashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/PatientDashboard.css'; // Import the CSS file for styling
 import PrescriptionPopup from './PrescriptionPopup';
+
 /*import { Link } from 'react-router-dom';*/
 
 const PatientDashboard = () => {
@@ -31,7 +32,7 @@ const PatientDashboard = () => {
           <h1 className="title">Patient Bridge</h1>
         </div>
         <div className="header-actions">
-        <button className="track-button">Track Medication</button>
+          <button className="track-button">Track Medication</button>
           <button className="prescriptions-button" onClick={openPopup}>
             Prescriptions
           </button>
